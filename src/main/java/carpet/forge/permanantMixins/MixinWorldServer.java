7ca3afcb1cc952698390c7bad6baa612968f8c8c
DeadlyMC <modding.deadlymc@gmail.com>
2019-01-25 16:28:38 +0530

    Clean Up
    Refactored mixin configs and removed unnecessary code from MixinWorldServer

---
+++ src/main/java/carpet/forge/permanantMixins/MixinWorldServer.java
@@ -0,0 +1,9 @@
+package carpet.forge.permanantMixins;
+
+import net.minecraft.world.WorldServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WorldServer.class)
+public abstract class MixinWorldServer{
+
+}
