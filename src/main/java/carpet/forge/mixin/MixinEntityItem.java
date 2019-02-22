381e3ece4130194145c4c163781c55a221137391
DeadlyMC <modding.deadlymc@gmail.com>
2019-02-22 17:27:58 +0530

    replace an overwrite

--- src/main/java/carpet/forge/mixin/MixinEntityItem.java
+++ src/main/java/carpet/forge/mixin/MixinEntityItem.java
@@ -6,114 +6,51 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EntityItem.class)
-public abstract class MixinEntityItem extends Entity {
-
-    @Shadow public abstract ItemStack getItem();
-
-    @Shadow private int pickupDelay;
-
-    @Shadow private int age;
-
-    public MixinEntityItem(World worldIn) {
+public abstract class MixinEntityItem extends Entity
+{
+    
+    @Shadow
+    private int pickupDelay;
+    @Shadow
+    private int age;
+    
+    public MixinEntityItem(World worldIn)
+    {
         super(worldIn);
     }
-
-    /**
-     * @author DeadlyMC
-     * @reason StackableShulkerBoxes
-     */
-    @Overwrite
-    public boolean combineItems(EntityItem other)
+    
+    @Shadow
+    public abstract ItemStack getItem();
+    
+    @Inject(method = "combineItems", at = @At(value = "RETURN", ordinal = 7, shift = At.Shift.BEFORE),
+            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void stackShulkers(EntityItem other, CallbackInfoReturnable<Boolean> cir, ItemStack itemstack, ItemStack itemstack1)
     {
-        if (other == (EntityItem)(Object)this)
+        if (((IItemStack) (Object) itemstack1).isGroundStackable() && ((IItemStack) (Object) itemstack).isGroundStackable())
         {
-            return false;
-        }
-        else if (other.isEntityAlive() && this.isEntityAlive())
-        {
-            ItemStack itemstack = this.getItem();
-            ItemStack itemstack1 = other.getItem();
-
-            if (this.pickupDelay != 32767 && other.pickupDelay != 32767)
-            {
-                if (this.age != -32768 && other.age != -32768)
-                {
-                    if (itemstack1.getItem() != itemstack.getItem())
-                    {
-                        return false;
-                    }
-                    else if (itemstack1.hasTagCompound() ^ itemstack.hasTagCompound())
-                    {
-                        return false;
-                    }
-                    else if (itemstack1.hasTagCompound() && !itemstack1.getTagCompound().equals(itemstack.getTagCompound()))
-                    {
-                        return false;
-                    }
-                    else if (itemstack1.getItem() == null)
-                    {
-                        return false;
-                    }
-                    else if (itemstack1.getItem().getHasSubtypes() && itemstack1.getMetadata() != itemstack.getMetadata())
-                    {
-                        return false;
-                    }
-                    else if (itemstack1.getCount() < itemstack.getCount())
-                    {
-                        return other.combineItems((EntityItem)(Object)this);
-                    }
-                    else if (itemstack1.getCount() + itemstack.getCount() > itemstack1.getMaxStackSize())
-                    {
-                        // [FCM] Add check for stacking shoulkers without NBT on the ground
-                        if (((IItemStack)(Object) itemstack1).isGroundStackable() && ((IItemStack)(Object) itemstack).isGroundStackable())
-                        {
-                            itemstack1.grow(itemstack.getCount());
-                            other.pickupDelay = Math.max(other.pickupDelay, this.pickupDelay);
-                            other.age = Math.min(other.age, this.age);
-                            other.setItem(itemstack1);
-                            this.setDead();
-                            return true;
-                        }
-                        return false;
-                    }
-                    // [FCM] Make sure stackable items are checked before combining them, always true in vanilla
-                    else if (!itemstack1.isStackable() && !itemstack.isStackable())
-                    {
-                        return false;
-                    }
-                    else if (!itemstack.areCapsCompatible(itemstack1))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        itemstack1.grow(itemstack.getCount());
-                        other.pickupDelay = Math.max(other.pickupDelay, this.pickupDelay);
-                        other.age = Math.min(other.age, this.age);
-                        other.setItem(itemstack1);
-                        this.setDead();
-                        return true;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
+            itemstack1.grow(itemstack.getCount());
+            other.pickupDelay = Math.max(other.pickupDelay, this.pickupDelay);
+            other.age = Math.min(other.age, this.age);
+            other.setItem(itemstack1);
+            this.setDead();
+            cir.setReturnValue(true);
         }
     }
-
-
+    
+    @Redirect(method = "combineItems", at = @At(value = "INVOKE",
+              target = "Lnet/minecraft/item/ItemStack;areCapsCompatible(Lnet/minecraft/item/ItemStack;)Z"))
+    private boolean modifyCapsCompat(ItemStack itemstack, ItemStack other)
+    {
+        return !itemstack.areCapsCompatible(other) || !other.isStackable() && !itemstack.isStackable();
+    }
+    
+    
 }
