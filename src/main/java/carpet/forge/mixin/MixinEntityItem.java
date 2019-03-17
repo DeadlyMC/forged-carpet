241de0797552597cbb0658a14adbef9fc2b85f77
DeadlyMC <modding.deadlymc@gmail.com>
2019-03-17 17:11:32 +0530

    fixed crash with tweakeroo

--- src/main/java/carpet/forge/mixin/MixinEntityItem.java
+++ src/main/java/carpet/forge/mixin/MixinEntityItem.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(EntityItem.class)
+@Mixin(value = EntityItem.class, priority = 900)
 public abstract class MixinEntityItem extends Entity
 {
     
