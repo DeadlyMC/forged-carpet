febc8eab47c2c241742021a56b132cb82838119a
DeadlyMC <modding.deadlymc@gmail.com>
2020-11-28 16:30:19 +0530

    Better `ctrlQCraftingFix` mixin

--- src/main/java/carpet/forge/mixin/ContainerMixin.java
+++ src/main/java/carpet/forge/mixin/ContainerMixin.java
@@ -1,7 +1,6 @@
 package carpet.forge.mixin;
 
 import carpet.forge.CarpetSettings;
-import carpet.forge.helper.CtrlQCrafting;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Container;
@@ -11,7 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -22,40 +20,22 @@ public abstract class ContainerMixin
     @Shadow
     public List<Slot> inventorySlots;
     
-    @Inject(method = "slotClick", at = @At("HEAD"), cancellable = true)
-    private void onSlotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player, CallbackInfoReturnable<ItemStack> cir)
+    @Shadow public abstract ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player);
+    
+    @Shadow public abstract void detectAndSendChanges();
+    
+    @Inject(method = "slotClick", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/Slot;decrStackSize(I)Lnet/minecraft/item/ItemStack;", ordinal = 2), cancellable = true)
+    private void ctrlQCrafting(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player, CallbackInfoReturnable<ItemStack> cir)
     {
-        if (clickTypeIn == ClickType.THROW && CarpetSettings.ctrlQCraftingFix && player.inventory.getItemStack().isEmpty() && slotId >= 0)
+        if (CarpetSettings.ctrlQCraftingFix && slotId == 0 && dragType == 1)
         {
-            ItemStack itemStack = ItemStack.EMPTY;
-            Slot slot = inventorySlots.get(slotId);
-            if (slot != null && slot.canTakeStack(player))
+            Slot slot = this.inventorySlots.get(slotId);
+            while (slot.getHasStack())
             {
-                if (slotId == 0 && dragType == 1)
-                {
-                    ItemStack itemStackDropAll = CtrlQCrafting.dropAllCrafting(player, slotId, inventorySlots);
-                    while (!itemStackDropAll.isEmpty() && ItemStack.areItemsEqual(slot.getStack(), itemStackDropAll))
-                    {
-                        itemStack = itemStackDropAll.copy();
-                        itemStackDropAll = CtrlQCrafting.dropAllCrafting(player, slotId, inventorySlots);
-                    }
-                    cir.setReturnValue(itemStack);
-                    cir.cancel();
-                }
+                this.slotClick(slotId, 0, ClickType.THROW, player);
             }
+            this.detectAndSendChanges();
+            cir.setReturnValue(ItemStack.EMPTY);
         }
     }
-    
-    /*
-    // [FCM] CtrlQCrafting(Recipe book support) - Check if item stack is stackable
-    @Redirect(
-            method = "slotClick",
-            at = @At(value = "INVOKE", ordinal = 0,
-                    target = "Lnet/minecraft/item/ItemStack;areItemStackTagsEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z")
-    )
-    private boolean isAreItemStackTagsEqual(ItemStack stackA, ItemStack stackB)
-    {
-        return ItemStack.areItemStackTagsEqual(stackA, stackB) && stackB.isStackable();
-    }
-    */
 }
