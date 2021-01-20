7b6b87a1ba538305e0075e1db5f592a3d2ded2c1
Deadly <modding.deadlymc@gmail.com>
2021-01-20 20:15:07 +0530

    space

--- src/main/java/carpet/forge/utils/Reference.java
+++ src/main/java/carpet/forge/utils/Reference.java
@@ -3,7 +3,7 @@ package carpet.forge.utils;
 public class Reference {
 
     public static final String MOD_ID = "forgedcarpet";
-    public static final String MOD_NAME = "ForgedCarpet";
+    public static final String MOD_NAME = "Forged Carpet";
     public static final String MOD_VERSION = "1.2.1";
     public static final String ACCEPTED_MC_VERSIONS = "1.12.2";
     public static final String FINGERPRINT = "@FINGERPRINT@";
