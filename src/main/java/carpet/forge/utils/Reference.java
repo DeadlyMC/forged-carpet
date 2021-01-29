14b66672411cdcf3cca0785b15431aab282cd7b4
Deadly <modding.deadlymc@gmail.com>
2021-01-29 13:02:02 +0530

    1.3.1 Version bump

--- src/main/java/carpet/forge/utils/Reference.java
+++ src/main/java/carpet/forge/utils/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "forgedcarpet";
     public static final String MOD_NAME = "Forged Carpet";
-    public static final String MOD_VERSION = "1.3.0";
+    public static final String MOD_VERSION = "1.3.1";
     public static final String ACCEPTED_MC_VERSIONS = "1.12.2";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/DeadlyMC/forged-carpet/master/update.json";
